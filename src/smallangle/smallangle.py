3cb9a500a6d5c7d0e22f8e56bd8d9c0b982c02c9
Niels Verduijn <117174966+Young-Nilla@users.noreply.github.com>
2024-11-15 14:47:50 +0100

    smallangle aanpassen

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,14 +4,35 @@ from numpy import pi
 import pandas as pd
 
 
+@click.group()
+def cmd_group():
+    pass
+
+@cmd_group.command("sin")
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+    help=".",
+    show_default=True,  # show default in help
+    type = int
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@cmd_group.command("tan")
+@click.option(
+    "-n",
+    "--number",
+    default=1,
+    help=".",
+    show_default=True,  # show default in help
+    type = int
+)
 def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
