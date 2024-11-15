f88b00c580f7f531d5d85496d78e6384fb44f49e
Niels Verduijn <117174966+Young-Nilla@users.noreply.github.com>
2024-11-15 15:48:19 +0100

    Smallangle docstrings

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -13,11 +13,15 @@ def cmd_group():
     "-n",
     "--number",
     default=1,
-    help=".",
+    help="The number of points to calculate within the range [0, 2π].",
     show_default=True,  # show default in help
     type = int
 )
 def sin(number):
+    """
+    This command generates 'number' within the range [0, 2π]
+    and calculates their sine values. The result are displayed as a table.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -27,11 +31,15 @@ def sin(number):
     "-n",
     "--number",
     default=1,
-    help=".",
+    help="The number of points to calculate within the range [0, 2π].",
     show_default=True,  # show default in help
     type = int
 )
 def tan(number):
+    """
+    This command generates 'number' within the range [0, 2π]
+    and calculates their tangent values. The result are displayed as a table.
+    """
     x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
